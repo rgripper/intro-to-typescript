074dff2bf4c45e02cd447ef9182e9b52bd3bc8f3
Vladimir Zotov <azuresplash@gmail.com>
2019-01-08 22:46:39 +1100

    Added null and undefined chapter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { hexToRgb, rgbToHex } from "./color-functions";
+import { hexToRgb, rgbToHex } from "./chapter - function-type/color-functions";
 
 import "./styles.css";
 
