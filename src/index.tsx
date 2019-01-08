4cde48ca300762aa73e89667ac8f4e60ad101ae9
Vladimir Zotov <azuresplash@gmail.com>
2019-01-08 23:00:27 +1100

    Updated jest setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { hexToRgb, rgbToHex } from "./color-functions";
 
 import "./styles.css";
-
-export let color = {
-  r: 255,
-  g: 0,
-  b: 0,
-  get hex(): string {
-    return rgbToHex(this.r, this.g, this.b);
-  },
-  set hex(hex: string) {
-    let { r, g, b } = hexToRgb(hex);
-    this.r = r;
-    this.g = g;
-    this.b = b;
-  }
-};
 
 function App() {
   return (
