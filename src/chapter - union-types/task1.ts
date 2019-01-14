739e1ced3f2b111a0069cb00f68ad03285116f3a
Vladimir Zotov <azuresplash@gmail.com>
2019-01-15 08:33:13 +1100

    Added a task for union types

---
+++ src/chapter - union-types/task1.ts
@@ -0,0 +1,21 @@
+/*
+  type ChatEvent = ???
+  
+  fields
+  - type 
+    possible values: 
+    - "UserJoined"
+    - "UserLeft"
+    - "MessagePublished"
+  - user
+    fields
+    - name
+    - avatarUrl
+  - message
+    fields
+    - text
+    - imageUrl
+    - videoUrl
+    - senderId
+  - userId
+*/
