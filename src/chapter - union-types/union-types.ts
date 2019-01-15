4a4f0c797a9254454b507b95aeae84bf14c03c07
Vladimir Zotov <azuresplash@gmail.com>
2019-01-15 22:15:32 +1100

    Added union types example

---
+++ src/chapter - union-types/union-types.ts
@@ -0,0 +1,26 @@
+type Result = Success | Failure;
+
+type Success = {
+  isSuccess: true;
+  value: number;
+}
+
+type Failure = {
+  isSuccess: false;
+  errorMessage: string;
+}
+
+function convertStringToNumber (input: string): Result  {
+  if (input === 'one') return { isSuccess: true, value: 0 }
+  else if(input === 'two') return { isSuccess: true, value: 1 }
+  else return { isSuccess: false, errorMessage: 'Must be a string!' }
+}
+
+const one = convertStringToNumber('one');
+const two = convertStringToNumber('rubbish');
+
+//if (one.isSuccess && two.isSuccess) {
+  const sum = one.value + two.value;
+  console.log(sum);
+//}
+
