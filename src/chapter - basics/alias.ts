950682ba94a5c1d05882f26eb6003ae60f4b1182
Gary Yige Sun <yige.sun@accenture.com>
2019-01-17 23:58:19 +1100

    add live exampes to chapter - basics

--- src/chapter - basics/alias.ts
+++ src/chapter - basics/alias.ts
@@ -2,6 +2,30 @@
 type N = Number;
 const age: N = 28;
 
+//hiding complex types behind an alias
+enum Subject {
+  MachineLearning,
+  AI,
+  KnowledgeBase,
+  Algorithms
+}
+
+type Student = {
+  id: number;
+  name: string;
+  gender: string;
+  course: string;
+  subjects: Subject[];
+};
+
+// const student: Student = {
+//   id: 12345,
+//   name: "Gary Sun",
+//   gender: "male",
+//   course: "IT",
+//   subjects: [Subject.AI, Subject.Algorithms]
+// };
+
 //these types can be exported to be consumed in other file
 export { N };
 
